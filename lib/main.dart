c8c2cb8e29346a80c2ba1cdef09fded5c78f4ac2
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-06-05 00:13:56 +0300

    - added images & svgs folders for assets - added flutter_svg & added flutter_screenutils - make a file for doc app client - added Navigation extension - added routing folder that contains router class and routes file - added colors & text styels - added empty login screen - added onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DocDoc',
-      theme: ThemeData(useMaterial3: true),
-    );
-  }
+  runApp(DocApp(appRouter: AppRouter()));
 }
